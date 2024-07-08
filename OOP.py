a33d738ac45b924e3ebd873563a14d282e62361c
rezaazimibar <www.beta04@gmail.com>
2024-07-08 19:22:06 +0330

    practice part 1

--- OOP.py
+++ OOP.py
@@ -1,3 +1,15 @@
+# -----------------------------------------OOP------------------------------------
+
+class MyClass:
+    pass
+
+
+obj1 = MyClass()
+obj2 = MyClass()
+
 print(type(3432))
 print(type("hey hey"))
 print(type(True))
+print(f"this is the main class with out () '{type(MyClass)}'")
+print(f"this is the obj from class:         '{type(obj1)}'")
+print(f"this is the another obj from class: '{type(obj2)}'")
