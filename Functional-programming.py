b13656fb2e98a1f1b3d4cedb7ec664b22e80c9f1
rezaazimibar <www.beta04@gmail.com>
2024-07-14 10:28:16 +0330

    functional programming part 2

--- Functional-programming.py
+++ Functional-programming.py
@@ -48,14 +48,75 @@
 # print(list(zip(my_list, your_dict, his_tuple)))
 
 # -------------------------------reduce-()--------------------------------
-from functools import reduce  # we should call it at top of code
+# from functools import reduce  # we should call it at top of code
+#
+# mylist = [5, 6, 7]
+#
+#
+# def my_accumulator(acc, item):
+#     print(acc, item)
+#     return acc + item
+#
+#
+# print(reduce(my_accumulator, mylist, 5))
 
-mylist = [5, 6, 7]
+# -------------------------------lambda-expressions-------------------------
 
+# from functools import reduce
+#
+# print(list(map(lambda item: item * 5, [2, 3, 4, 5, 6])))
+#
+# print(list(filter(lambda item: item % 5 == 0, [10, 2, 15, 20, 3, 4, 1, 9])))
+#
+# print(reduce(lambda acc, item: item + acc, [23, 34], 0))
 
-def my_accumulator(acc, item):
-    print(acc, item)
-    return acc + item
+# -----------------------------------exercise--------------------------------
 
+# my_list = [2, 3, 4, 5]
+# print(list(map(lambda item: item ** 2, my_list)))
+#
+# my_tuple = [(1, 2), (2, 1), (8, 9), (10, 4)]
+#
+# my_tuple.sort(key=lambda item: item[1])
+# print(my_tuple)
 
-print(reduce(my_accumulator, mylist, 5))
+# -----------------------------comprehensions-----------------------------
+
+# # we can use it on list,set,dictionary
+#
+# my_list = [char for char in "hello there"]
+# my_list1 = [item for item in range(1, 10)]
+# my_list2 = [item ** 5 for item in range(1, 10)]
+# my_list3 = [item * 7 for item in range(1, 15) if item % 2 != 0]
+#
+# print(my_list)
+# print(my_list1)
+# print(my_list2)
+# print(f"this going to print just odd numbers: {my_list3}")
+#
+# # when we want to use it on set
+#
+# my_set = {char for char in range(1, 10)}
+# my_set1 = {item for item in "hey hey"}
+#
+# print(my_set)
+# print(my_set1)
+#
+# # when we want to use it on dictionary
+# my_dict = {
+#     'a': 2,
+#     'b': 3,
+#     'c': 5
+# }
+#
+# my_dict1 = {key: value + 5 for key, value in my_dict.items()}
+# my_dict2 = {num: num + 2 for num in [2, 3, 4, 5, 6]}
+# print(my_dict1)
+# print(my_dict2)
+
+# ---------------------------------exercise--------------------------------
+
+# my_list = ["d", "d", "m", "w", "c", "m"]
+#
+# my_list1 = list({item for item in my_list if my_list.count(item) > 1})
+# print(my_list1)
