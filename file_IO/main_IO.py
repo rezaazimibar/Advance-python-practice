65187266c90790f6ea067d3f84dad6eecd4e26f8
rezaazimibar <www.beta04@gmail.com>
2024-07-29 13:15:00 +0330

    IO translate project done

--- file_IO/main_IO.py
+++ file_IO/main_IO.py
@@ -14,13 +14,46 @@
 #
 # my_txt.close()  # close the file that we opened top of code
 
-with open('my_txt.txt', mode='a') as my_file:  # standard way to open a IO file
-    # mode=r+ mean we can write and read the file or r mean we can only read a file
-    # w means we can only write in file
-    # a means never write letter from first
-    text = my_file.write("new text new life")
-    print(text)
+# with open('my_txt.txt', mode='a') as my_file:  # standard way to open a IO file
+#     # mode=r+ mean we can write and read the file or r mean we can only read a file
+#     # w means we can only write in file
+#     # a means never write letter from first
+#     text = my_file.write("new text new life")
+#     print(text)
+#
+# with open('my_txt2.txt', mode='w') as my_file2:  # create a new txt file-
+#     # -or overwrite the existing file
+#     text = my_file2.write("new text in new txt file")
 
-with open('my_txt2.txt,', mode='w') as my_file2:  # create a new txt file-
-    # -or overwrite the existing file
-    text = my_file2.write("new text in new txt file")
+# ---------------------------------------------File-Paths------------------------------------------
+
+# with open('../modules/python1.py', mode='r') as my_pyfile:
+#     print(my_pyfile.read())
+
+# ----------------------------------------------Error-in-IO----------------------------------------
+
+# try:
+#     with open('./my_txt.txt', mode='x') as my_file:  # x is a wierd mode that not exist
+#         print(my_file.read())
+#
+# except FileNotFoundError as err:
+#     print(f"file not found the error name is {err}")
+#     raise err
+# except IOError as err:  # this error mean the machine can not read the text with any reason
+#     print(f"file not readable the error name is {err}")
+
+# ----------------------------------------------exercise-------------------------------------------
+
+from translate import Translator
+
+translator = Translator(to_lang="de")
+
+try:
+    with open("my_txt2.txt", mode='r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        print(translation)
+        with open('./summer_file.txt', mode='w') as my_summer:
+            my_summer.write(translation)
+except FileNotFoundError as e:
+    print("file not found")
