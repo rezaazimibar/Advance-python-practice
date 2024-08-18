13cf4590e9da8f29118e6a076e9321b77db5ac5d
rezaazimibar <www.beta04@gmail.com>
2024-08-18 18:38:39 +0330

    test part 1

--- test/main.py
+++ test/main.py
@@ -1,2 +1,19 @@
 def do_stuff(num):
-    return num + 5
+    try:
+        return int(num) + 5
+    except ValueError as err:
+        return err
+
+
+def lower(num):
+    try:
+        if num:
+            return int(num) + 5
+        else:
+            return "please enter a number"
+    except ValueError as err:
+        return err
+
+
+def adder(num1, num2):
+    return num1 + num2
--- test/test.py
+++ test/test.py
@@ -4,10 +4,43 @@ import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print("this is a set up function")
     def test_do_stuff(self):
-        num = 10
-        result = main.do_stuff(num)
+        test_param = 10
+        result = main.do_stuff(test_param)
         self.assertEqual(result, 15)
 
+    def test_do_stuff2(self):
+        '''just random command'''
+        test_param = 'Hey'
+        result = main.do_stuff(test_param)
+        self.assertIsInstance(result, ValueError)
 
-unittest.main()
+    def test_lower(self):
+        test_param = None
+        result = main.lower(test_param)
+        self.assertEqual(result, "please enter a number")
+
+    def test_lower1(self):
+        test_param = ''
+        result = main.lower(test_param)
+        self.assertEqual(result, "please enter a number")
+
+    def test_low2(self):
+        test_param = 0
+        result = main.lower(test_param)
+        self.assertEqual(result, "please enter a number")
+
+    def test_adder(self):
+        test_param = 10
+        test_param2 = 10
+        result = main.adder(test_param, test_param2)
+        self.assertEqual(result, 20)
+
+    def tearDown(self):
+        print("run end of each test")
+
+
+if __name__ == "__main__":
+    unittest.main()
