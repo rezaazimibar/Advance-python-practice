185696254c1b180920c7849696f5c060a9a6ab0b
rezaazimibar <www.beta04@gmail.com>
2024-08-19 12:03:02 +0330

    test part 2

--- test/main.py
+++ test/main.py
@@ -1,19 +1,45 @@
-def do_stuff(num):
-    try:
-        return int(num) + 5
-    except ValueError as err:
-        return err
+# def do_stuff(num):
+#     try:
+#         return int(num) + 5
+#     except ValueError as err:
+#         return err
+#
+#
+# def lower(num):
+#     try:
+#         if num:
+#             return int(num) + 5
+#         else:
+#             return "please enter a number"
+#     except ValueError as err:
+#         return err
+#
+#
+# def adder(num1, num2):
+#     return num1 + num2
+
+# -------------------------------------------exercise----------------------------------------------
+
+import random
 
 
-def lower(num):
-    try:
-        if num:
-            return int(num) + 5
-        else:
-            return "please enter a number"
-    except ValueError as err:
-        return err
+def game_func(answer, guss):
+    if 0 < guss < 11:
+        if guss == answer:
+            print("you are smart")
+            return True
+    else:
+        print("select another number!!!")
+        return False
 
 
-def adder(num1, num2):
-    return num1 + num2
+my_answer = random.randint(1, 10)
+if __name__ == "__main__":
+    while True:
+        try:
+            my_guss = int(input("enter a number:"))
+            if game_func(my_answer, my_guss):
+                break
+        except ValueError:
+            print("enter a correct number!!")
+            continue
--- test/test.py
+++ test/test.py
@@ -1,45 +1,71 @@
+# import unittest
+#
+# import main
+#
+#
+# class TestMain(unittest.TestCase):
+#     def setUp(self):
+#         print("this is a set up function")
+#     def test_do_stuff(self):
+#         test_param = 10
+#         result = main.do_stuff(test_param)
+#         self.assertEqual(result, 15)
+#
+#     def test_do_stuff2(self):
+#         '''just random command'''
+#         test_param = 'Hey'
+#         result = main.do_stuff(test_param)
+#         self.assertIsInstance(result, ValueError)
+#
+#     def test_lower(self):
+#         test_param = None
+#         result = main.lower(test_param)
+#         self.assertEqual(result, "please enter a number")
+#
+#     def test_lower1(self):
+#         test_param = ''
+#         result = main.lower(test_param)
+#         self.assertEqual(result, "please enter a number")
+#
+#     def test_low2(self):
+#         test_param = 0
+#         result = main.lower(test_param)
+#         self.assertEqual(result, "please enter a number")
+#
+#     def test_adder(self):
+#         test_param = 10
+#         test_param2 = 10
+#         result = main.adder(test_param, test_param2)
+#         self.assertEqual(result, 20)
+#
+#     def tearDown(self):
+#         print("run end of each test")
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
+
+# --------------------------------------------exercise---------------------------------------------
+
 import unittest
 
 import main
 
 
-class TestMain(unittest.TestCase):
-    def setUp(self):
-        print("this is a set up function")
-    def test_do_stuff(self):
-        test_param = 10
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, 15)
+class GameTest(unittest.TestCase):
+    def test_game1(self):
+        answer = 3
+        guss = 3
+        result = main.game_func(answer, guss)
+        self.assertTrue(result)
 
-    def test_do_stuff2(self):
-        '''just random command'''
-        test_param = 'Hey'
-        result = main.do_stuff(test_param)
-        self.assertIsInstance(result, ValueError)
+    def test_game2(self):
+        result = main.game_func(5, 2)
+        self.assertFalse(result)
 
-    def test_lower(self):
-        test_param = None
-        result = main.lower(test_param)
-        self.assertEqual(result, "please enter a number")
-
-    def test_lower1(self):
-        test_param = ''
-        result = main.lower(test_param)
-        self.assertEqual(result, "please enter a number")
-
-    def test_low2(self):
-        test_param = 0
-        result = main.lower(test_param)
-        self.assertEqual(result, "please enter a number")
-
-    def test_adder(self):
-        test_param = 10
-        test_param2 = 10
-        result = main.adder(test_param, test_param2)
-        self.assertEqual(result, 20)
-
-    def tearDown(self):
-        print("run end of each test")
+    def test_game3(self):
+        result = main.game_func(3, 22222)
+        self.assertFalse(result)
 
 
 if __name__ == "__main__":
